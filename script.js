07b4185c75a062e1d87b44010010dfa6247a1688
targolghasemi <targolghasemi85@gmail.com>
2025-10-22 17:22:13 +0330

    js edited 2

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const addItem = document.getElementById('add-item');
+const todoList = document.getElementById('todo-list');
 
 
 let items = [];
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let todoInput = document.getElementById('input').value;
+
+    if (todoInput === '') return;
+    addToDom(todoInput);
+    saveToLocalStorage(todoInput);
+})
 
 //adding tasks
 function addToDom(text) {
@@ -16,4 +27,44 @@ function addToDom(text) {
     btn.textContent = "Delete";
     btn.classList.add('delete-btn'); 
 
+    btn.addEventListener('click' , () => {
+        li.remove();
+        removeTodo(text);
+    });
+    li.append(span , btn);
+    todoList.appendChild(li);
 }
+
+//store to localStorage
+function saveToLocalStorage(todo) {
+    items.push(todo);
+    localStorage.setItem('todos' , JSON.stringify(items));
+}
+
+//loading todos
+function loadTodos() {
+    const stored = localStorage.getItem('todos');
+    items = stored ? JSON.parse(stored) : [];
+    items.forEach((t) => addToDom(t));
+};
+
+
+//remove from localStorage
+function removeTodo(todo) {
+    const stored = localStorage.getItem('todos');
+    if (stored) {
+        items = JSON.parse(stored)
+    };
+    const newItems = [];
+    for (let i = 0; i < items.length; i++) {
+        if (items[i] !== todo) {
+            newItems.push(items[i]);
+        };
+    };
+
+    localStorage.setItem('todos' , JSON.stringify(newItems));
+
+};
+
+loadTodos();
+
